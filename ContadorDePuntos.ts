b78e06f51ea949b65753c6f985066b7ef10e9004
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-01 19:46:02 -0300

    aas

--- ContadorDePuntos.ts
+++ ContadorDePuntos.ts
@@ -3,44 +3,30 @@ interface puntos{
     puntos: number;
 }
 
-window.onload = () => {
-
-    let EquiposA: puntos = {
-        puntos : 10
-    };
-    
-    let EquiposB: puntos = {
-        puntos : 10
-    };
-    
-    // Obtener el elemento por su ID
-    const CEquiposA = document.getElementById('EquipoA');
-    const CEquiposB = document.getElementById('EquipoB');
-  
-    let InputPuntosB = document.getElementById('InputPuntosB')as HTMLInputElement;
-    let InputPuntosA = document.getElementById('InputPuntosA')as HTMLInputElement;
-
-    const botonConvertir = document.getElementById('btnConvertir');
-
-    if (botonConvertir &&  InputPuntosB &&  InputPuntosA && CEquiposA && CEquiposB) {
-        botonConvertir.addEventListener('click', () => {
-          // Obtener el valor del input (siempre es un string)
-          const valorInput: string = inputElement.value;
-    
-          // Convertir el valor a número
-          const numero: number = parseInt(valorInput, 10); // O puedes usar Number(valorInput) o +valorInput
-    
-          // Verificar si la conversión fue exitosa
-          if (isNaN(numero)) {
-            CEquiposA.textContent = "Por favor, ingresa un número válido.";
-            CEquiposA.textContent = "Por favor, ingresa un número válido.";
-          } else {
-            CEquiposA.textContent = `${numero}`;
-            CEquiposB.textContent = `${numero}`;
-          }
-        });
-      }
-    
+let EquiposA: puntos = {
+  puntos : 0
 };
 
+let EquiposB: puntos = {
+  puntos : 0
+};
 
+function contar()
+{
+  console.log('Click');
+
+  const CEquiposA = document.getElementById('PuntosA') as HTMLElement;
+  const CEquiposB = document.getElementById('PuntosB') as HTMLElement;
+  
+  let InputPuntosB = document.getElementById('InputPuntosB')as HTMLInputElement;
+  let InputPuntosA = document.getElementById('InputPuntosA')as HTMLInputElement;
+
+  EquiposA.puntos += parseInt(InputPuntosA.value);
+  EquiposB.puntos += parseInt(InputPuntosB.value); 
+
+  
+  CEquiposA.innerText += EquiposA.puntos;
+  CEquiposB.innerText += EquiposB.puntos;
+
+
+}

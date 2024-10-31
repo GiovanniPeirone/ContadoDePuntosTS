efe3b61da578b9c064f44b708113bef104cb6aab
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-31 09:46:43 -0300

    asd

--- ContadorDePuntos.ts
+++ ContadorDePuntos.ts
@@ -1,4 +1,5 @@
 
+
 interface puntos{
   punto : number;
 }
@@ -12,24 +13,31 @@ let EquipoB : puntos = {
   punto: 0
 }
 
+// Defino los ID del HTML
 
+// Id del Equipo A
 const HandBall_Input_Equipo_A = document.getElementById('HandBall_Input_Equipo_A') as HTMLInputElement;
 const Resistencia_Input_A = document.getElementById('Resistencia_Input_A') as HTMLInputElement;
 const Ajederz_Input_A = document.getElementById('Ajederz_Input_A') as HTMLInputElement;
 
+// Id del EquipoB
 const HandBall_Input_Equipo_B = document.getElementById('HandBall_Input_Equipo_B') as HTMLInputElement;
 const Resistencia_Input_B = document.getElementById('Resistencia_Input_B') as HTMLInputElement;
 const Ajederz_Input_B = document.getElementById('Ajederz_Input_B') as HTMLInputElement;
 
+// Id que va a Mostrar los resultados de las sumas de puntos
 const OutputPuntosA = document.getElementById('PuntosA') as HTMLElement;
 const OutputPuntosB = document.getElementById('PuntosB') as HTMLElement;
 
+// Id que declara que equipo gano
 const EquipoGanador = document.getElementById('EquipoGanador') as HTMLElement;
 
 
-
+// Funcion que suma toos los puntos
 function contar(): void {
   
+  // Comvierte los string en Enteros
+  //el || 0 declara que si no hay valor lo comvierta a 0
   let Valor_A_Ingresado_HandBall : number = parseInt(HandBall_Input_Equipo_A.value, 10) || 0;
   let Valor_A_Ingresado_Rsistencia : number = parseInt(Resistencia_Input_A.value, 10) || 0;
   let Valor_A_Ingresado_Ajedrez : number = parseInt(Ajederz_Input_A.value, 10) || 0;
